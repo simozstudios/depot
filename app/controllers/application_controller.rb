9ce8507ef4a5bd46e414f9f813889c0865ac2306
Mike <mike@simozstudios.com>
2011-05-23 18:52:07 -0700

    Added authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_filter :authorize
   protect_from_forgery
 
   private
-
+	
+	def authorize
+	  unless User.find_by_id(session[:user_id])
+	    redirect_to login_url, :notice => "Please log in"
+	  end
+	end
+	
     def current_cart 
       Cart.find(session[:cart_id])
     rescue ActiveRecord::RecordNotFound
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Depot::Application.initialize!
+
+Depot::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'baci.lindsaar.net',
+    :user_name            => '<username>',
+    :password             => '<password>',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
+  end
